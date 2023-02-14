001d528b8d0ce739a1a413a9a0dd1c3e0f8512a7
Shivam Bhatt <shivambhatt97@gmail.com>
2023-02-14 20:30:54 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/issue_tracker');
+// mongoose.connect('mongodb://localhost/issue_tracker');
+mongoose.connect('process.env.DB_URL');
 
 const db =mongoose.connection;
 
